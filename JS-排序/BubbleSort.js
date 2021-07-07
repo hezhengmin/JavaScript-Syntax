3af6be6f5a84982ecfbe058076681bd9e6a18061
hezhengmin <zhengmin099@gmail.com>
2021-07-07 23:24:25 +0800

    add-JS bubble sort

--- JS-排序/BubbleSort.js
+++ JS-排序/BubbleSort.js
@@ -1,6 +1,6 @@
 Array.prototype.bubble_sort = function () {
     for (let i = 0; i < this.length; i++) {
-        for (let j = 0; j < this.length - i - 1; j++) {
+        for (let j = 0; j < this.length-i-1; j++) {
             if (this[j] > this[j + 1]) {
                 let temp = this[j];
                 this[j] = this[j + 1];
