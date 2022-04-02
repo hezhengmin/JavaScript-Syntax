6b9335c318c028d13ad52b1b964b168159cb4602
hezhengmin <zhengmin099@gmail.com>
2022-04-02 19:35:25 +0800

    $refs

---
+++ Vue/範例/VueCli/vue_test/02_sre_ref/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1 v-text="msg" ref="title"></h1>
+        <button ref="btn" @click="showDOM">showDOM</button>
+        <School ref="sch" />
+    </div>
+</template>
+
+<script>
+//引入school組件
+import School from "./components/School";
+export default {
+    name: "App",
+    data() {
+        return {
+            msg: "Message",
+        };
+    },
+    components: {
+        School,
+    },
+    methods: {
+        showDOM() {
+            console.log(this.$refs.title);
+            console.log(this.$refs.btn);
+            console.log(this.$refs.sch); //VueComponent組件實例對象
+        },
+    },
+};
+</script>
+
+<style></style>
---
+++ Vue/範例/VueCli/vue_test/02_sre_ref/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="demo">
+        <h2>{{ name }}</h2>
+        <h2>{{ address }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SchoolName",
+    data() {
+        return {
+            name: "Minghe",
+            address: "台中市",
+        };
+    },
+};
+</script>
+
+<style>
+.demo {
+    color: blue;
+}
+</style>
