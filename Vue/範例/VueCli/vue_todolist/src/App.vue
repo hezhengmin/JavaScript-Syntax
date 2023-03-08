e1f3915dbd5d6a0d9e9ef051dc073ab14edff7cb
hezhengmin <zhengmin099@gmail.com>
2023-03-08 22:59:12 +0800

    init todolist範本

--- Vue/範例/VueCli/vue_todolist/src/App.vue
+++ Vue/範例/VueCli/vue_todolist/src/App.vue
@@ -1,12 +1,50 @@
 <template>
-    <div id="app"></div>
+    <div id="app">
+        <h1>Todo List</h1>
+        <input type="text" v-model="newTodo" placeholder="Add a new todo" />
+        <button v-on:click="addTodo">Add</button>
+        <ul>
+            <li v-for="(todo, index) in todos" v-bind:class="{ done: todo.done }">
+                <span>{{ todo.text }}</span>
+                <button v-on:click="removeTodo(index)">X</button>
+                <button v-on:click="toggleDone(index)">{{ todo.done ? "Undone" : "Done" }}</button>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
     name: "App",
     components: {},
+    data() {
+        return {
+            newTodo: "",
+            todos: [],
+        }
+    },
+    methods: {
+        addTodo: function () {
+            if (this.newTodo.trim() !== "") {
+                this.todos.push({
+                    text: this.newTodo,
+                    done: false,
+                })
+                this.newTodo = ""
+            }
+        },
+        removeTodo: function (index) {
+            this.todos.splice(index, 1)
+        },
+        toggleDone: function (index) {
+            this.todos[index].done = !this.todos[index].done
+        },
+    },
 }
 </script>
 
-<style></style>
+<style scoped>
+.done {
+    text-decoration: line-through;
+}
+</style>
