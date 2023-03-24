5695381953a6fb2eefe5607d59d6ba0c777bf08c
hezhengmin <zhengmin099@gmail.com>
2023-03-24 23:07:54 +0800

    默認插槽slot、102 尚硅谷Vue技术 默认插槽

---
+++ Vue/範例/VueCli/vue_slots/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app" class="container">
+        <MyCategory title="教學">
+            <img src="@/assets/logo.png" alt="" />
+        </MyCategory>
+        <MyCategory title="美食">
+            <ul>
+                <li v-for="(item, index) in foods" :key="index">{{ item }}</li>
+            </ul>
+        </MyCategory>
+        <MyCategory title="電影">
+            <video controls src=""></video>
+        </MyCategory>
+
+        <MyCategory title=""></MyCategory>
+    </div>
+</template>
+
+<script>
+import MyCategory from "./components/MyCategory";
+
+export default {
+    name: "App",
+    components: {
+        MyCategory,
+    },
+    data() {
+        return {
+            foods: ["炒飯", "水餃", "火鍋"],
+        };
+    },
+};
+</script>
+
+<style>
+.container {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
---
+++ Vue/範例/VueCli/vue_slots/src/components/MyCategory.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <div class="category">
+        <h2>{{ title }}</h2>
+        <slot>默認插槽slot，沒寫回顯示這段文字</slot>
+    </div>
+</template>
+<script>
+export default {
+    name: "MyCategory",
+    props: ["title"],
+};
+</script>
+<style>
+.category {
+    background: rgb(111, 111, 202);
+    width: 200px;
+    height: 300px;
+}
+h2 {
+    text-align: center;
+    background: orange;
+}
+img {
+    width: 100%;
+}
+video {
+    width: 100%;
+}
+</style>
