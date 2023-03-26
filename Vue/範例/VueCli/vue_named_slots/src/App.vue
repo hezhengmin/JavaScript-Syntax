a1a4691746f3fa5cd41cf98fb0aa04098b044a8c
hezhengmin <zhengmin099@gmail.com>
2023-03-26 22:23:04 +0800

    103 尚硅谷Vue技术 具名插槽

---
+++ Vue/範例/VueCli/vue_named_slots/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="app" class="container">
+        <MyCategory title="教學">
+            <img slot="center" src="@/assets/logo.png" alt="" />
+            <a slot="footer" href="https://hezhengmin.github.io/">網站連結</a>
+        </MyCategory>
+        <MyCategory title="美食">
+            <ul slot="center">
+                <li v-for="(item, index) in foods" :key="index">{{ item }}</li>
+            </ul>
+            <div class="footer" slot="footer">
+                <a href="https://hezhengmin.github.io/">雞排連結</a>
+                <a href="https://hezhengmin.github.io/">熱狗連結</a>
+            </div>
+        </MyCategory>
+        <MyCategory title="電影">
+            <video slot="center" controls src=""></video>
+            <template v-slot:footer>
+                <div class="footer">
+                    <a href="https://hezhengmin.github.io/">經典</a>
+                    <a href="https://hezhengmin.github.io/">動作</a>
+                    <a href="https://hezhengmin.github.io/">動作</a>
+                </div>
+                <h4>歡迎收看</h4>
+            </template>
+        </MyCategory>
+
+        <MyCategory title=""></MyCategory>
+    </div>
+</template>
+
+<script>
+import MyCategory from "./components/MyCategory";
+
+export default {
+    name: "App",
+    components: {
+        MyCategory,
+    },
+    data() {
+        return {
+            foods: ["炒飯", "水餃", "火鍋"],
+        };
+    },
+};
+</script>
+
+<style>
+.container,
+.footer {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
