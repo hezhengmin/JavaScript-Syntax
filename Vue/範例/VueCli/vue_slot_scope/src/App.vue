705870e6ebf46ab1f30f74cae7705648b9e47591
hezhengmin <zhengmin099@gmail.com>
2023-03-27 23:10:53 +0800

    scope、slot-scope

---
+++ Vue/範例/VueCli/vue_slot_scope/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="app" class="container">
+        <!-- 讓父組件向子組件指定位置插入html結構 -->
+        <MyCategory title="遊戲">
+            <template scope="array">
+                {{ array }}
+            </template>
+        </MyCategory>
+        <MyCategory title="遊戲">
+            <template scope="{games}">
+                <ul>
+                    <li v-for="(item, index) in games" :key="index">
+                        {{ item }}
+                    </li>
+                </ul>
+            </template>
+        </MyCategory>
+
+        <MyCategory title="遊戲">
+            <template scope="{games,msg}">
+                <ol>
+                    <li v-for="(item, index) in games" :key="index">
+                        {{ item }}
+                    </li>
+                </ol>
+                <h4>{{ msg }}</h4>
+            </template>
+        </MyCategory>
+
+        <MyCategory title="遊戲">
+            <template slot-scope="{ games }">
+                <h4 v-for="(item, index) in games" :key="index">
+                    {{ item }}
+                </h4>
+            </template>
+        </MyCategory>
+    </div>
+</template>
+
+<script>
+import MyCategory from "./components/MyCategory";
+
+export default {
+    name: "App",
+    components: {
+        MyCategory,
+    },
+    data() {
+        return {
+            foods: ["炒飯", "水餃", "火鍋"],
+        };
+    },
+};
+</script>
+
+<style>
+.container,
+.footer {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
---
+++ Vue/範例/VueCli/vue_slot_scope/src/components/MyCategory.vue
@@ -0,0 +1,39 @@
+<template lang="">
+    <div class="category">
+        <h2>{{ title }}</h2>
+        <slot :games="games" msg="Hello"></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: "MyCategory",
+    props: ["title"],
+    data() {
+        return {
+            games: [
+                "薩爾達傳說 王國之淚",
+                "even if TEMPEST 黃昏中魔女如是說",
+                "十三支演義 偃月三國傳 1・2 for Nintendo Switch",
+                "VARIABLE BARRICADE 百密一疏少女心 NS",
+            ],
+        };
+    },
+};
+</script>
+<style>
+.category {
+    background: rgb(111, 111, 202);
+    width: 200px;
+    height: 300px;
+}
+h2 {
+    text-align: center;
+    background: orange;
+}
+img {
+    width: 100%;
+}
+video {
+    width: 100%;
+}
+</style>
