9c943cb4485fe2e054d0e145be60e32b71074b8b
hezhengmin <zhengmin099@gmail.com>
2023-07-03 23:27:37 +0800

    async、await應用、vue的$refs應用

---
+++ Vue/範例/VueCli/vue_refs/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <child-component ref="childRef"></child-component>
+        <button @click="callChildMethod">Call child method</button>
+    </div>
+</template>
+
+<script>
+import ChildComponent from "./components/ChildComponent.vue";
+
+export default {
+    name: "App",
+    components: {
+        ChildComponent,
+    },
+    methods: {
+        async callChildMethod() {
+            const sec = await this.$refs.childRef.resolveAfterNumberSeconds(5);
+            console.log(`父組件得到的sec為: ${sec}`);
+            this.$refs.childRef.childMethod();
+        },
+    },
+};
+</script>
+
+<style></style>
---
+++ Vue/範例/VueCli/vue_refs/src/components/ChildComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button @click="childMethod">Click me</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["data"],
+    methods: {
+        childMethod() {
+            console.log("Child method called");
+        },
+        resolveAfterNumberSeconds(sec) {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    console.log(`等${sec}秒時間到後~`);
+                    resolve(sec);
+                }, sec * 1000);
+            });
+        },
+    },
+};
+</script>
