a8082b0f60faad6587df675cb343383cc56eae7f
hezhengmin <zhengmin099@gmail.com>
2023-07-20 23:33:24 +0800

    v-slot 子傳父元件 (類似slot prop)

--- Vue/範例/VueCli/vue_scoped_slots/src/App.vue
+++ Vue/範例/VueCli/vue_scoped_slots/src/App.vue
@@ -1,9 +1,27 @@
+<!--
+ * @Author: hezhengmin zhengmin099@gmail.com
+ * @Date: 2023-07-19 21:58:15
+ * @LastEditors: hezhengmin zhengmin099@gmail.com
+ * @LastEditTime: 2023-07-20 23:29:46
+ * @FilePath: \JavaScript-Syntax\Vue\範例\VueCli\vue_scoped_slots\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
     <div id="app">
-        <LightBox v-slot:test="props">
-            <ul>
-                <li v-for="(item, index) in props.options">{{ item.name }}/{{ item.val }}</li>
-            </ul>
+        <select v-model="lang">
+            <option v-for="item in langOptions" :value="item.val">{{ item.name }}</option>
+        </select>
+        <LightBox :lang="lang">
+            <!-- v-slot:default="slotProps" 或 v-slot="slotProps" 寫法都可以，不帶參數默認插槽 -->
+            <template v-slot="slotProps">
+                {{ slotProps.moneyLangString }}
+            </template>
+            <!-- 子元件透過 selectLangString 丟往父元件存取，類似prop 
+            { selectLangString } <----ES6解構賦值
+            -->
+            <template v-slot:lightBoxSlotName="{ selectLangString }">
+                {{ selectLangString }}
+            </template>
         </LightBox>
     </div>
 </template>
@@ -16,14 +34,21 @@ export default {
     components: {
         LightBox,
     },
+    data() {
+        return {
+            langOptions: [
+                { name: "繁體中文", val: "tw" },
+                { name: "日本語", val: "jp" },
+                { name: "English", val: "en" },
+            ],
+            lang: "tw",
+        };
+    },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
--- Vue/範例/VueCli/vue_scoped_slots/src/components/LightBox.vue
+++ Vue/範例/VueCli/vue_scoped_slots/src/components/LightBox.vue
@@ -2,25 +2,37 @@
  * @Author: hezhengmin zhengmin099@gmail.com
  * @Date: 2023-07-19 22:42:58
  * @LastEditors: hezhengmin zhengmin099@gmail.com
- * @LastEditTime: 2023-07-19 23:20:42
+ * @LastEditTime: 2023-07-20 23:27:54
  * @FilePath: \JavaScript-Syntax\Vue\範例\VueCli\vue_scoped_slots\src\components\LightBox.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template lang="">
     <div>
-        <slot name="test" :options="langOptions"></slot>
+        <slot :moneyLangString="moneyString[lang]"></slot>
+        <slot name="lightBoxSlotName" :selectLangString="langString[lang]"></slot>
     </div>
 </template>
 <script>
 export default {
     name: "LightBox",
+    props: {
+        lang: {
+            type: String,
+            default: "tw",
+        },
+    },
     data() {
         return {
-            langOptions: [
-                { name: "繁體中文", val: "tw" },
-                { name: "日本語", val: "jp" },
-                { name: "English", val: "en" },
-            ],
+            langString: {
+                tw: "你好",
+                jp: "こんにちは",
+                en: "Hello",
+            },
+            moneyString: {
+                tw: "TWD",
+                jp: "JPY",
+                en: "USD",
+            },
         };
     },
 };
