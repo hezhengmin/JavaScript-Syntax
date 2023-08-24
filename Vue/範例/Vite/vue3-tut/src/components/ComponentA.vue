e57d1ecf3146b3e594dd4786bf9ecec01c0282ff
hezhengmin <zhengmin099@gmail.com>
2023-08-24 23:22:56 +0800

    vue3使用ref雙向綁定

---
+++ Vue/範例/Vite/vue3-tut/src/components/ComponentA.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hello World</h1>
+    <!-- 單向綁定 -->
+    <p>{{ data }}</p>
+    <!-- 雙向綁定 -->
+    <input type="text" v-model="data2" />
+    <p>{{ data2 }}</p>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const data = "feature";
+const data2 = ref("test");
+</script>
+
+<style scoped></style>
