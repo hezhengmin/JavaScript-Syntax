9e0b8b1e63731051f1be8e173d0f679d46b25c70
hezhengmin <zhengmin099@gmail.com>
2023-10-18 23:28:47 +0800

    defineProps、defineEmits 應用

---
+++ Vue/範例/Vite/vue3-tut/src/components/ChildProduct.vue
@@ -0,0 +1,22 @@
+<template lang="">
+    <p>產品Id:{{ props.productId }}</p>
+    <p>產品名稱:{{ props.productName }}</p>
+    <input type="text" v-model="newProductName" />
+    <button @click="ChangeProductName">更新產品名字</button>
+</template>
+<script setup>
+import { reactive, ref } from "vue";
+
+const newProductName = ref("");
+const props = defineProps({
+    productId: Number,
+    productName: String,
+});
+
+const emits = defineEmits(["update", "submit"]);
+
+const ChangeProductName = () => {
+    emits("update", newProductName.value);
+};
+</script>
+<style lang=""></style>
---
+++ Vue/範例/Vite/vue3-tut/src/views/DefinePropsAndDefineEmits.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <ChildProduct :productId="777" :productName="productName" @update="updateProductName"></ChildProduct>
+</template>
+<script setup>
+import { reactive, ref } from "vue";
+import ChildProduct from "../components/ChildProduct.vue";
+const productName = ref("蘋果汁");
+const productId = ref(777);
+
+const updateProductName = (newProductName) => {
+    console.log(newProductName);
+    productName.value = newProductName;
+};
+</script>
+<style lang=""></style>
