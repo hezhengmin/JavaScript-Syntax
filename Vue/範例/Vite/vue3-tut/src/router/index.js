7aea2e676838869ce3a6686d59275eec54e55ca3
hezhengmin <zhengmin099@gmail.com>
2023-10-24 23:36:06 +0800

    computed用法

--- Vue/範例/Vite/vue3-tut/src/router/index.js
+++ Vue/範例/Vite/vue3-tut/src/router/index.js
@@ -43,6 +43,7 @@ const routes = [
     { path: "/DefinePropsAndDefineEmits", component: () => import("../views/DefinePropsAndDefineEmits.vue") },
     { path: "/ProvideAndInject", component: () => import("../views/ProvideAndInject.vue") },
     { path: "/SuspensePage", component: () => import("../views/SuspensePage.vue") },
+    { path: "/ComputedPage", component: () => import("../views/ComputedPage.vue") },
 ];
 
 // 3. 创建路由实例并传递 `routes` 配置
---
+++ Vue/範例/Vite/vue3-tut/src/views/ComputedPage.vue
@@ -0,0 +1,22 @@
+<template lang="">
+    書本數量：{{ count }}
+    <button @click="addBook">新增書本</button>
+</template>
+
+<script setup>
+import { ref, reactive, computed } from "vue";
+
+const author = reactive({
+    name: "John Doe",
+    books: ["Vue 2 - Advanced Guide", "Vue 3 - Basic Guide", "Vue 4 - The Mystery"],
+});
+
+const count = computed(() => {
+    return author.books.length;
+});
+
+const addBook = () => {
+    author.books.push("Book");
+};
+</script>
+<style lang=""></style>
