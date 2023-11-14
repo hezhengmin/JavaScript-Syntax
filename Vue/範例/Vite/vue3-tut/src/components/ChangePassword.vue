219677a9e4284a594321d4696a9224523fecd448
hezhengmin <zhengmin099@gmail.com>
2023-11-14 22:57:04 +0800

    新增vue-simple-password-meter應用

--- Vue/範例/Vite/vue3-tut/src/components/ChangePassword.vue
+++ Vue/範例/Vite/vue3-tut/src/components/ChangePassword.vue
@@ -1,19 +1,39 @@
 <template lang="">
-    <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" label-width="120px" class="demo-ruleForm">
-        <el-form-item label="密碼" prop="pass">
-            <el-input v-model="ruleForm.pass" type="password" show-password autocomplete="off" />
-        </el-form-item>
-        <el-form-item label="確認密碼" prop="checkPass">
-            <el-input v-model="ruleForm.checkPass" type="password" show-password autocomplete="off" />
-        </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="submitForm(ruleFormRef)">Submit</el-button>
-            <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
-        </el-form-item>
-    </el-form>
+    <el-row>
+        <el-col :span="8" :offset="8">
+            <el-form
+                ref="ruleFormRef"
+                :model="ruleForm"
+                status-icon
+                :rules="rules"
+                label-width="120px"
+                class="demo-ruleForm">
+                <el-form-item label="密碼" prop="pass">
+                    <div style="margin-bottom: 1px">
+                        <el-input
+                            class="w-50"
+                            v-model="ruleForm.pass"
+                            type="password"
+                            show-password
+                            autocomplete="off" />
+                        <password-meter :password="ruleForm.pass" />
+                    </div>
+                </el-form-item>
+                <el-form-item label="確認密碼" prop="checkPass">
+                    <el-input v-model="ruleForm.checkPass" type="password" show-password autocomplete="off" />
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm(ruleFormRef)">Submit</el-button>
+                    <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
+        <el-col :span="8"></el-col>
+    </el-row>
 </template>
 <script setup>
 import { reactive, ref } from "vue";
+import PasswordMeter from "vue-simple-password-meter";
 
 const ruleFormRef = ref();
 const ruleForm = reactive({
@@ -32,8 +52,6 @@ const validatePass = (rule, value, callback) => {
         if (value.length < 8 || !hasLowerCase || !hasUpperCase || !hasDigit || !hasSpecialChar) {
             callback(new Error("要求密碼長度至少為8個字符，必須包含大小寫字母、數字和特殊符號的組合。"));
         }
-
-        console.log(ruleFormRef.value);
 
         if (ruleForm.checkPass !== "") {
             if (!ruleFormRef.value) return;
@@ -75,4 +93,41 @@ const resetForm = (formEl) => {
     formEl.resetFields();
 };
 </script>
-<style lang=""></style>
+<style lang="scss" scoped>
+.w-50 {
+    width: 12.5rem;
+}
+// https://github.com/miladd3/vue-simple-password-meter
+// 製訂密碼強度樣式
+.po-password-strength-bar {
+    border-radius: 2px;
+    transition: all 0.2s linear;
+    height: 5px;
+    margin-top: 2px;
+}
+
+.po-password-strength-bar.risky {
+    background-color: #f95e68;
+    width: 10%;
+}
+
+.po-password-strength-bar.guessable {
+    background-color: #fb964d;
+    width: 32.5%;
+}
+
+.po-password-strength-bar.weak {
+    background-color: #fdd244;
+    width: 55%;
+}
+
+.po-password-strength-bar.safe {
+    background-color: #b0dc53;
+    width: 77.5%;
+}
+
+.po-password-strength-bar.secure {
+    background-color: #35cc62;
+    width: 100%;
+}
+</style>
