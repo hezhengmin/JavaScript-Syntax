88d647226f65e6ff322e37ec277a4d4ac58928e9
hezhengmin <zhengmin099@gmail.com>
2023-11-20 23:18:59 +0800

    组件 v-model

---
+++ Vue/範例/Vite/vue3-tut/src/components/CustomInput.vue
@@ -0,0 +1,8 @@
+<template><input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" /></template>
+<script setup>
+//props 名稱必須是 modelValue
+defineProps(["modelValue"]);
+//emit 事件的名稱必須是 update:modelValue
+defineEmits(["update:modelValue"]);
+</script>
+<style lang=""></style>
--- Vue/範例/Vite/vue3-tut/src/components/Parent.vue
+++ Vue/範例/Vite/vue3-tut/src/components/Parent.vue
@@ -2,15 +2,25 @@
     <h1>Parent</h1>
     <input type="text" v-model="txt" />
     <Child :data="txt" @update="updateHandler"></Child>
+    <h1>组件 v-model</h1>
+    <CustomInput v-model="message" />
+    {{ message }}
+    <br />
+    <!--<CustomInput v-model="message" /> 等同於下方 -->
+    <CustomInput :modelValue="message" @update:modelValue="message = $event" />
 </template>
 
 <script setup>
 import Child from "../components/Child.vue";
+import CustomInput from "../components/CustomInput.vue";
+
 import { ref } from "vue";
 const txt = ref("hello");
 
 const updateHandler = (data) => {
     console.log(data);
 };
+
+const message = ref("自訂組件");
 </script>
 <style lang=""></style>
